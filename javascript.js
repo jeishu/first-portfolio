415102e0e34ffc6388bd06db642aa09e1a94a8ff
jeishu <jeremy.zhu@utexas.edu>
2020-10-21 18:14:55 -0500

    changes to scrolling, added js, added scroll to top, smooth scrolling

---
+++ javascript.js
@@ -0,0 +1,20 @@
+//Get the button
+var mybutton = document.getElementById("goTop");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("goTop").style.display = "block";
+  } 
+  else {
+    document.getElementById("goTop").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
