9427612bf331962895d8d1d39ddb211102bcdb57
jeishu <jeremy.zhu@utexas.edu>
2020-10-22 00:03:05 -0500

    big changed on about section, added scroll-to-top button back

--- js/scrolling-to-top.js
+++ js/scrolling-to-top.js
@@ -1,20 +1,20 @@
 // // Get the button
-// var mybutton = document.getElementById("goTop");
+var mybutton = document.getElementById("goTop");
 
 // // When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()};
 
-// function scrollFunction() {
-//   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100 ){
-//     document.getElementById("goTop").style.display = "block";
-//   } 
-//   else {
-//     document.getElementById("goTop").style.display = "none";
-//   }
-// }
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100 ){
+    document.getElementById("goTop").style.display = "block";
+  } 
+  else {
+    document.getElementById("goTop").style.display = "none";
+  }
+}
 
 // // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//   document.body.scrollTop = 0;
-//   document.documentElement.scrollTop = 0;
-// }
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
